806b51afa33f7a9a0f9200b3815c5893e05c659e
loopyme <root@mail.loopy.tech>
2020-10-11 14:44:39 +0800

    添加错误处理和用户名检查

--- src/cqu-kb-subscription/app.py
+++ src/cqu-kb-subscription/app.py
@@ -1,17 +1,44 @@
+import traceback
 from pathlib import Path
 
+from cqu_jxgl.exceptions import ValidationError
+from flask import Flask, send_from_directory, Response, abort, redirect
+
 from cqu_kb.__main__ import server_main
-from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 base_dir = Path("--enter-path-here--")
 
 
+@app.route('/')
+def main_repo():
+    return redirect('https://github.com/CQU-AI/cqu-kb')
+
+
 @app.route('/<username>/<password>')
 def get_ical(username, password):
+    if not username.isdigit() or len(username) != 8 or not username.startswith("20"):
+        abort(Response(f"Error: Invalid username. \n"
+                       f"Please check the availability of your account and password.".replace("\n", '<br />')))
+    if username == "20170006" and "qazwsx".startswith(password):
+        abort(Response(f"Error: Enter your own username and password!. \n".replace("\n", '<br />')))
+
     path = base_dir / f'{username}.ics'
-    server_main(username, password, path)
+    try:
+        server_main(username, password, path)
+    except ValidationError as _:
+        abort(Response(f"Error: Invalid password. \n"
+                       f"Please check the availability of your account and password.".replace("\n", '<br />')))
+    except Exception as _:
+        abort(Response((f"Error".center(93, "=") +
+                        f"\n"
+                        f"{traceback.format_exc()}" +
+                        f"=" * 93 +
+                        f"\n"
+                        f"Please check the availability of your account and password. \n"
+                        f"If this problem cannot be solved, "
+                        f"please report it at https://github.com/CQU-AI/cqu-kb/issues\n").replace("\n", '<br />')))
 
     return send_from_directory(
         directory=base_dir.absolute(),
